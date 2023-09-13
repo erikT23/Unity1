786c3701b22582a3d136c96190f11d423530d622
Erik Tapia Guzman <84143122+erikT23@users.noreply.github.com>
2023-09-13 17:27:46 -0600

    13-09

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+   [SerializeField] private Rigidbody rbComponent;
+   [SerializeField] private float jumpForce;
+   [SerializeField] private float minyPos;
+   [SerializeField] private float maxyPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isGrounded = transform.position.y > minyPos && transform.position.y < maxyPos;
+        if(Input.GetKeyDown(KeyCode.Space)&& isGrounded) {
+            rbComponent.AddForce(0f,jumpForce,0f);
+        }
+    }
+}
